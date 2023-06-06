63af6881816267318435009b7ade7a333baa8760
Oscar Davies <oscar.william.davies@outlook.com>
2023-06-06 08:44:54 +0100

    Seperate Code

---
+++ BestRust/PlayerHistory.cs
@@ -0,0 +1,43 @@
+﻿namespace BestRust
+{
+    internal class PlayerHistory
+    {
+        public DataPoint[] data;
+
+        public HistoryStats calculateStats()
+        {
+            var stats = new HistoryStats();
+
+            foreach (var item in data)
+            {
+                stats.avg += item.attributes.value;
+
+                if (stats.min > item.attributes.min) stats.min = item.attributes.min;
+
+                if (stats.max < item.attributes.max) stats.max = item.attributes.max;
+            }
+
+            stats.avg /= data.Length;
+
+            return stats;
+        }
+    }
+
+    internal class HistoryStats
+    {
+        public int avg = 0;
+        public int max = 0, min = 10000;
+    }
+
+    internal struct DataPoint
+    {
+        public string type;
+        public DataAttribute attributes;
+    }
+
+    internal struct DataAttribute
+    {
+        public DateTime timestamp;
+        public int max, value, min;
+    }
+}
---
+++ BestRust/Server.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+
+namespace BestRust
+{
+    internal class Server
+    {
+        public string id;
+        public ServerAttributes attributes;
+
+        public bool GetHistory(TimeSpan period, out PlayerHistory? result)
+        {
+            result = null;
+
+            var now = DateTime.Now;
+            var then = now - period;
+
+            string url = $"https://api.battlemetrics.com/servers/{id}/player-count-history?start={then:yyyy-MM-dd}T00:00:00.000Z&stop={now:yyyy-MM-dd}T00:00:00.000Z&resolution=60";
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage(new HttpMethod("GET"), url))
+                {
+                    var response = httpClient.Send(request);
+
+                    if (!response.IsSuccessStatusCode)
+                        return false;
+
+                    var contentTask = response.Content.ReadAsStringAsync();
+                    contentTask.Wait();
+
+                    var body = contentTask.Result;
+                    result = JsonConvert.DeserializeObject<PlayerHistory>(body);
+
+                    return true;
+                }
+            }
+        }
+    }
+
+    internal struct ServerAttributes
+    {
+        public string name, address, ip;
+        public int port, rank;
+    }
+}
